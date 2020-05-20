518942bfac357d5b91bb5e4d63162aa5d6c16707
wangzheqin <cetrinw@sina.com>
2020-05-20 17:33:47 +0800

    rebuild

---
+++ springboot-service-demo/src/test/java/com/cetrinw/springbootservicedemo/SpringbootServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cetrinw.springbootservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- springboot-web-demo/src/main/java/com/cetrinw/mvc/TestController.java
+++ springboot-web-demo/src/main/java/com/cetrinw/mvc/TestController.java
@@ -1,10 +1,11 @@
 package com.cetrinw.mvc;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TestController {
 
     @RequestMapping("/test")
